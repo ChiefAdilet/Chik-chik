005684d3227d38d9fedbdd321c247e9685a2ab0c
ChiefAdilet <a.a.adilet@mail.ru>
2023-06-08 23:20:43 +0600

    commit

--- JS/script.js
+++ JS/script.js
@@ -15,26 +15,57 @@ const startSlider = () => {
     let activeSlide = 1;
     let position = 0;
 
+    const chekSlider = () => {
+        if ((activeSlide + 2 === sliderItems.length && 
+            document.documentElement.offsetWidth > 560) ||
+                activeSlide === sliderItems.length) {
+            btnNextSlide.style.display = 'none';
+        } else {
+            btnNextSlide.style.display = '';
+        }
+
+        if (activeSlide ===1) {
+            btnPrevSlide.style.display = 'none';
+        } else {
+            btnPrevSlide.style.display = '';
+        }
+    }
+
+    chekSlider();
+
     const nextSlide = () => {
-        sliderItems[activeSlide].classList.remove('slider__item-active');
+        sliderItems[activeSlide]?.classList.remove('slider__item-active');
         position = -sliderItems[0].clientWidth * activeSlide;
 
         sliderList.getElementsByClassName.transform = 'translateX(${position}px)';
         activeSlide += 1;
-        sliderItems[activeSlide].classList.remove('slider__item-active');
+        sliderItems[activeSlide]?.classList.remove('slider__item-active');
+        chekSlider();
     }
 
     const prevSlide = () => {
-        sliderItems[activeSlide].classList.remove('slider__item-active');
+        sliderItems[activeSlide]?.classList.remove('slider__item-active');
         position = -sliderItems[0].clientWidth * (activeSlide - 2);
 
         sliderList.getElementsByClassName.transform = 'translateX(${position}px)';
         activeSlide -= 1;
-        sliderItems[activeSlide].classList.remove('slider__item-active');
+        sliderItems[activeSlide]?.classList.remove('slider__item-active');
+        chekSlider();
     }
 
     btnPrevSlide.addEventListener('click', prevSlide);
     btnNextSlide.addEventListener('click', nextSlide);
+
+    window.addEventListener('resize', () => {
+        if (activeSlide + 2 > sliderItems.length && documentElement.offsetWidth > 560) {
+            activeSlide = sliderItems.length - 2;
+            sliderItems[activeSlide]?.classList.add('slider__item-active');
+        }
+
+        position = -sliderItems[0].clientWidth * (activeSlide - 1);
+        sliderList.getElementsByClassName.transform = 'translateX(${position}px)';
+        chekSlider();
+    })
 }
 
 const initSlider = () => {
