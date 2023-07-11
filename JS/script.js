acda2d102a8b80dde94d434ee1b39eba6d858d17
Adilet Amangeldi <a.a.adilet@mail.ru>
2023-07-11 10:33:18 +0600

    commit

--- JS/script.js
+++ JS/script.js
@@ -77,7 +77,7 @@ const initSlider = () => {
 
     window.addEventListener('load', () => {
         sliderContainer.getElementsByClassName.display = ''
-        addPreload(slider)
+        removePreload(slider)
         
         startSlider()
     });
